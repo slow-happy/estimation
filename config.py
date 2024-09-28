4d48185d4c24541451f288194541e71ab91d9ad3
Jeongsic <superpower@hanmail.net>
2024-09-29 08:07:57 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,11 @@ from pathlib import Path
 
 basedir = Path(__file__).parent.parent
 
-# user1 = "superpower"
-# pw1 = "mecha75"
+# user1 = ""
+# pw1 = ""
 # ip1 = "192.168.0.10"
 # db1 = "flask_web"
 # mysql_url = f"mysql+pymysql://{user1}:{pw1}@{ip1}/{db1}?charset=utf8"
-
-
-user1 = "superpower"
-pw1 = "mecha75"
-ip1 = "svc.sel5.cloudtype.app:31693"
-db1 = "flask_web"
-mysql_url = f"mysql+pymysql://{user1}:{pw1}@{ip1}/{db1}?charset=utf8"
-
 
 # BaseConfig 클리스 작성하기
 class BaseConfig:
